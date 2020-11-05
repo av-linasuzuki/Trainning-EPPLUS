a57a13674381b1dd5e56809703795504e6991b17
Lina <lina.akemi.suzuki@avanade.com>
2020-11-05 17:50:38 -0300

    FuncoesEPPLUS

---
+++ TrainningEpplusFramework/TrainningEpplusFramework/Entities/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainningEpplusFramework.Entities
+{
+    class Products
+    {
+        public int productId{ get; set; }
+        public string productName { get; set; }
+        public double productPriceUnit { get; set; }
+        public int productAmount { get; set; }
+        public double productSubtotal { get; set; }
+
+        public Products()
+        {
+
+        }
+        public Products(int prodId, string prodName, double prodPriceUnit, int prodAmount, double prodSubtotal)
+        {
+            this.productId = prodId;
+            this.productName = prodName;
+            this.productPriceUnit = prodPriceUnit;
+            this.productAmount = prodAmount;
+            this.productSubtotal = prodSubtotal;
+        }
+       
+    }
+}
---
+++ TrainningEpplusFramework/TrainningEpplusFramework/Program.cs
@@ -0,0 +1,53 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace TrainningEpplusFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // armazena o local que está planilha (não necessário inicialmente)
+            var path = @"\CadastrarProdutos.xlsx";
+
+            //para acessar uma planilha, criamos um pacote com o método ExcelPackage e acessamos com FileInfo
+            var package = new ExcelPackage(new FileInfo(path));
+
+            //para criar uma pasta de trabalho, criamos um pacote com o método ExcelPackage
+            //var package = new ExcelPackage();
+
+            /* para acessar a primeira planilha da pasta de trabalho, é preciso acessar o Workbook para então, 
+             * acessar as planilhas da pasta de trabalho */
+
+            // nesse caso, acessamos a planilha através do índice
+            var firstSheetIndex = package.Workbook.Worksheets[0];
+            Console.WriteLine("Nome da planilha recuperada: {0}", firstSheetIndex); // imprime o nome da planilha recuperada
+
+            // recuperando o nome da primeira planilha através do nome
+            var firstSheetName = package.Workbook.Worksheets["Cadastrar"];
+            Console.WriteLine("Nome da planilha: {0}", firstSheetName);
+
+            // recuperando o nome da primeira planilha através do método First
+            var SheetName = package.Workbook.Worksheets.First();
+            Console.WriteLine("Nome da planilha: {0}", SheetName);
+
+            // acessar o valor de uma célula específica
+            var valorCel = firstSheetName.GetValue(2, 4);
+            Console.WriteLine("Valor recuperado da linha 2 coluna 4: {0}", valorCel);
+
+            // adicionando uma nova planilha
+            var newSheet = package.Workbook.Worksheets.Add("NovaPlanilha");
+
+            // adicionando um novo valor em uma célula
+            newSheet.Cells[1, 1].Value = "Meu novo valor";
+            //formatando a célula
+            newSheet.Cells[1, 1].Style.Font.Bold = true;
+
+            // Salvando uma copia da pasta de trabalho
+            package.SaveAs(new FileInfo(@"\CopiaPastaTrabalho.xlsx"));
+
+        }
+    }
+}
